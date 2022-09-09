cbe6021c48ce089136838adc1cbfedc6ca71771c
Olga Sh <shei-olya@mail.ru>
2022-09-09 16:02:06 +0300

    Практическое задание,задача 41_2

---
+++ HM_06_1_2/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.");
+Console.Write("Введите число M: ");
+int m= int.Parse(Console.ReadLine());
+
+while (m <0)
+{
+    Console.WriteLine(" введите положительное число");
+    m = int.Parse(Console.ReadLine());
+}
+string n="";
+int CountPositive=0;
+for(int count=0; count<m; count++)
+{
+    Console.Write("Введите число: ");
+    int result = int.Parse(Console.ReadLine());
+    n=n+result;
+    if(result>0)
+    {
+    CountPositive++;
+    }
+}
+Console.WriteLine("Количество положительных чисел: "+CountPositive);
